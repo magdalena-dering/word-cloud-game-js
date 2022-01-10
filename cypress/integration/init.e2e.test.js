2b2adc597885f227a88da02f0f5f16aa5689e2f7
Magdalena Dering <magdalena.dering@brainhub.pl>
2022-01-10 11:01:35 +0100

    Write tests for the whole app

--- cypress/integration/init.e2e.test.js
+++ cypress/integration/init.e2e.test.js
@@ -1,9 +1,21 @@
-describe('Cypress', () => {
-  it('is working', () => {
-    expect(true).to.equal(true);
+describe('Test the app', () => {
+  it('log in', () => {
+    cy.visit('/');
+    cy.get('input').type('John Doe');
+    cy.get('button').contains('Log in').click();
   });
-
-  it('opens the app', () => {
-    cy.visit('http://localhost:3000');
+  it('click random words in game container', () => {
+    cy.get('[data-testid="word"]').eq(2).click();
+    cy.get('[data-testid="word"]').eq(3).click();
+    cy.get('[data-testid="word"]').eq(8).click();
+  });
+  it('check results', () => {
+    cy.get('button').contains('Check').click();
+  });
+  it('finish game', () => {
+    cy.get('button').contains('Finish game').click();
+  });
+  it('log out', () => {
+    cy.get('a').contains('Log out').click();
   });
 });
